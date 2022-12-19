137cb90d49563d754b1fd31b6c770e12222761f1
Fynn Boyer <fynn-boyer@web.de>
2022-12-19 23:53:40 +0100

    Adjust fetch cpp

--- src/Fetch.cpp
+++ src/Fetch.cpp
@@ -5,33 +5,60 @@
 Response fetch(const char* url, RequestOptions options) {
     // Parsing URL.
     Url parsedUrl = parseUrl(url);
-    
-    WiFiClientSecure client;
-    // Retry every 15 seconds.
-    client.setTimeout(15000);
+    Serial.printf("url %s\r\n", url);
+    Serial.printf("scheme %s\r\n", parsedUrl.scheme.c_str());
+    Serial.printf("username %s\r\n", parsedUrl.username.c_str());
+    Serial.printf("password %s\r\n", parsedUrl.password.c_str());
+    Serial.printf("host %s\r\n", parsedUrl.host.c_str());
+    Serial.printf("path %s\r\n", parsedUrl.path.c_str());
+    Serial.printf("afterPath %s\r\n", parsedUrl.afterPath.c_str());
+    Serial.printf("query %s\r\n", parsedUrl.query.c_str());
+    Serial.printf("fragment %s\r\n", parsedUrl.fragment.c_str());
+    Serial.printf("port %d\r\n", parsedUrl.port);
+
+    bool useHttps = parsedUrl.scheme == "https";
+    WiFiClient clientInsecure;
+    WiFiClientSecure clientSecure;
 
     // Set fingerprint if https.
-    if(parsedUrl.scheme == "https") {
+    if(useHttps) {
         #ifdef ESP8266
         if(options.fingerprint == "" && options.caCert == "") {
             DEBUG_FETCH("[INFO] No fingerprint or caCert is provided. Using the INSECURE mode for connection!");
-            client.setInsecure();
+            clientSecure.setInsecure();
         }
-        else if(options.caCert != "") client.setTrustAnchors(new X509List(options.caCert.c_str()));
-        else client.setFingerprint(options.fingerprint.c_str());
+        else if(options.caCert != "") clientSecure.setTrustAnchors(new X509List(options.caCert.c_str()));
+        else clientSecure.setFingerprint(options.fingerprint.c_str());
         #elif defined(ESP32)
         if(options.caCert == "") {
             DEBUG_FETCH("[INFO] No CA Cert is provided. Using the INSECURE mode for connection!");
-            client.setInsecure();
+            clientSecure.setInsecure();
         }
-        else client.setCACert(options.caCert.c_str());
+        else clientSecure.setCACert(options.caCert.c_str());
         #endif
     }
 
+    // Retry every 15 seconds.
+    uint16_t timeout = 15000;
+    if (useHttps) {
+        clientSecure.setTimeout(timeout);
+    } else {
+        clientInsecure.setTimeout(timeout);
+    }
+
     // Connecting to server.
-    while(!client.connect(parsedUrl.host.c_str(), parsedUrl.port)) {
-        delay(1000);
-        Serial.print(".");
+    if (useHttps) {
+        while(!clientSecure.connect(parsedUrl.host.c_str(), parsedUrl.port)) {
+            // TODO (FB): add timeout
+            delay(1000);
+            Serial.print(".");
+        }
+    } else {
+        while(!clientInsecure.connect(parsedUrl.host.c_str(), parsedUrl.port)) {
+            // TODO (FB): add timeout
+            delay(1000);
+            Serial.print(".");
+        }
     }
 
     // Forming request.
@@ -44,35 +71,108 @@ Response fetch(const char* url, RequestOptions options) {
     DEBUG_FETCH("-----REQUEST START-----\n%s\n-----REQUEST END-----", request.c_str());
 
     // Sending request.
-    client.print(request);
+    if (useHttps) {
+        clientSecure.print(request);
+    } else {
+        clientInsecure.print(request);
+    }
+
+    DEBUG_FETCH("Sent request");
 
     // Getting response headers.
     Response response;
-    for(int nLine = 1; client.connected(); nLine++) {
-        // Reading headers line by line.
-        String line = client.readStringUntil('\n');
-        // Parse status and statusText from line 1.
-        if(nLine == 1) {
-            response.status = line.substring(line.indexOf(" ")).substring(0, line.indexOf(" ")).toInt();
-            response.statusText = line.substring(line.indexOf(String(response.status)) + 4);
-            response.statusText.trim();
-            continue;
+    String getAll;
+    String getBody;
+
+    if (useHttps) {
+        for(int nLine = 1; clientSecure.connected(); nLine++) {
+            DEBUG_FETCH("Getting response");
+
+            // Reading headers line by line.
+            String line = clientSecure.readStringUntil('\n');
+            // Parse status and statusText from line 1.
+            if(nLine == 1) {
+                response.status = line.substring(line.indexOf(" ")).substring(0, line.indexOf(" ")).toInt();
+                response.statusText = line.substring(line.indexOf(String(response.status)) + 4);
+                response.statusText.trim();
+                continue;
+            }
+
+            response.headers += line + "\n";
+            // If headers end, move on.
+            if(line == "\r") break;
+        }
+    } else {
+
+        int timoutTimer = 2000;
+        long startTimer = millis();
+        boolean state = false;
+
+        while ((startTimer + timoutTimer) > millis()) {
+            Serial.print(".");
+            delay(100);
+            while (clientInsecure.available()) {
+                char c = clientInsecure.read();
+                // Serial.printf("%s", c);
+                if (c == '\n') {
+                    if (getAll.length()==0) {
+                        state=true;
+                    }
+                    getAll = "";
+                }
+                else if (c != '\r') {
+                    getAll += String(c);
+                }
+                if (state == true) {
+                    getBody += String(c);
+                }
+                startTimer = millis();
+            }
+
+            if (getBody.length() > 0) {
+                break;
+            }
         }
 
-        response.headers += line + "\n";
-        // If headers end, move on.
-        if(line == "\r") break;
+        // for(int nLine = 1; clientInsecure.connected(); nLine++) {
+        //     DEBUG_FETCH("Getting response");
+        //     // Reading headers line by line.
+        //     String line = clientInsecure.readStringUntil('\n');
+        //     DEBUG_FETCH("Read string until");
+
+        //     // Parse status and statusText from line 1.
+        //     if(nLine == 1) {
+        //         response.status = line.substring(line.indexOf(" ")).substring(0, line.indexOf(" ")).toInt();
+        //         response.statusText = line.substring(line.indexOf(String(response.status)) + 4);
+        //         response.statusText.trim();
+        //         continue;
+        //     }
+
+        //     response.headers += line + "\n";
+        //     // If headers end, move on.
+        //     if(line == "\r") break;
+        // }
     }
 
     DEBUG_FETCH("-----HEADERS START-----\n%s\n-----HEADERS END-----", response.headers.text().c_str());
 
     // Getting response body.
-    while(client.available()) {
-        response.body += client.readStringUntil('\n');
-    }
+    if (useHttps) {
+        while(clientSecure.available()) {
+            response.body += clientSecure.readStringUntil('\n');
+        }
 
-    // Stopping the client.
-    client.stop();
+        // Stopping the client.
+        clientSecure.stop();
+    } else {
+        // while(clientInsecure.available()) {
+        //     response.body += clientInsecure.readStringUntil('\n');
+        // }
+        response.body = getBody;
+
+        // Stopping the client.
+        clientInsecure.stop();
+    }
 
     return response;
 }
@@ -80,7 +180,7 @@ Response fetch(const char* url, RequestOptions options) {
 FetchClient fetch(const char* url, RequestOptions options, OnResponseCallback onResponseCallback) {
     // Parsing URL.
     Url parsedUrl = parseUrl(url);
-    
+
     WiFiClientSecure client;
     // Retry every 15 seconds.
     client.setTimeout(15000);
